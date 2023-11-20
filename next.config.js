1d4ed59f4ad57e2ad5f73c103fec91623f745a00
Luchs41 <kokobo6@gmail.com>
2023-11-20 14:10:20 +0900

    Fix : Proxy setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://hgm-main.p-e.kr/:path*'
+      },
+      {
+        source: '/ml/:path*',
+        destination: 'https://hgm-ml.p-e.kr/:path*'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
