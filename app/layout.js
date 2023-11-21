fbd4ddc9f2c7ec352bec66c46835abc37f489951
Luchs41 <kokobo6@gmail.com>
2023-11-21 22:23:18 +0900

    Fixed : 로고를 Navbar 내부로 넣음

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 //import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Image from 'next/image'
-
+import { Navbar } from 'react-bootstrap'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +16,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head></head>
       <body>
-        <Image src="/HAGIMA.svg" alt="Logo of Hagima" width={100} height={50} priority={true} />
+        <Navbar>
+          <Image src="/HAGIMA.svg" alt="Logo of Hagima" width={100} height={50} priority={true} />
+        </Navbar>
         {children}
       </body>
     </html>
