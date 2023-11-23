b48531f597c91b486fb487e4827896529d5d7ce3
Luchs41 <kokobo6@gmail.com>
2023-11-24 00:28:55 +0900

    Added : 로그아웃 함수 -> 쿠키에서 닉네임도 삭제

--- func/logout.js
+++ func/logout.js
@@ -2,9 +2,17 @@ import Cookies from "js-cookie"
 export default function logout() {
   try {
     Cookies.remove('uuid')
-    window.location.href = '/'
   }
   catch (e) {
     console.log(e)
   }
+
+  try {
+    Cookies.remove('nickname')
+  }
+  catch (e) {
+    console.log(e)
+  }
+
+  window.location.href = '/'
 }
