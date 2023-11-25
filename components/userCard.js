1ee5331d936c2c22b05125939a2a1b91d3c7f57f
Luchs41 <kokobo6@gmail.com>
2023-11-26 05:34:19 +0900

    Added : 유저 카드 수정

--- components/userCard.js
+++ components/userCard.js
@@ -1,12 +1,24 @@
 'use client'
-import { Card } from "react-bootstrap"
-export default function UserCard({ faceImg, nickname }) {
+import { Card, Button } from "react-bootstrap"
+import { useEffect, useState } from "react"
+import PinModal from "./PinModal"
+import NewVoiceModal from "./NewVoiceModal"
+
+export default function UserCard({ faceImg, nickname, uuid, index }) {
+  const [showPinModal, setShowPinModal] = useState(false)
+  const [showNewVoiceModal, setShowNewVoiceModal] = useState(false)
   return (
-    <Card style={{ width: '15vw' }}>
+    <Card style={{ width: '16vw' }}>
+      <NewVoiceModal show={showNewVoiceModal} handleClose={() => setShowNewVoiceModal(false)} />
+      <PinModal show={showPinModal} handleClose={() => setShowPinModal(false)} index={index} nickname={nickname} />
       <Card.Img variant="top" src={faceImg} style={{ width: '100%', height: '30vh' }} />
       <Card.Body>
-        <Card.Title>{nickname}</Card.Title>
+        <Card.Title style={{ textAlign: 'center' }}>{nickname}</Card.Title>
       </Card.Body>
+      <Card.Footer>
+        <Button variant='secondary' style={{ width: '100%', fontSize: '10px' }} onClick={() => setShowNewVoiceModal(true)}>음성 설정</Button>
+        <Button variant='secondary' style={{ width: '100%', fontSize: '10px' }} onClick={() => setShowPinModal(true)}>PIN 로그인</Button>
+      </Card.Footer>
     </Card>
   )
 }
