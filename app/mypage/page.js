448af2162fe3ec66b8294bbb0bd480532f3829cf
Luchs41 <kokobo6@gmail.com>
2023-11-26 14:55:48 +0900

    Fixed : API 대기 중 Spinner 추가

--- app/mypage/page.js
+++ app/mypage/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form, Spinner } from 'react-bootstrap';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
@@ -13,7 +13,7 @@ export default function Mypage() {
   const [showNewUserModal, setShowNewUserModal] = useState(false);
   const [showFaceRecogModal, setShowFaceRecogModal] = useState(false);
   const [showNewVoiceModal, setShowNewVoiceModal] = useState(false);
-
+  const [showSpinner, setShowSpinner] = useState(true);
   // State for user data
   const [uuid, setUuid] = useState(null)
   useEffect(() => {
@@ -30,44 +30,50 @@ export default function Mypage() {
     instance.get('/main/profile')
       .then(res => {
         setUserArray(res.data.data)
+        setShowSpinner(false)
       })
       .catch(err => console.log(err))
   }, [showNewUserModal])
-
-  return (
+  if (showSpinner) return (
     <Container>
-      <NewUserModal show={showNewUserModal} handleClose={() => setShowNewUserModal(false)} />
-      <FaceRecogModal show={showFaceRecogModal} handleClose={() => setShowFaceRecogModal(false)} userArray={userArray} />
       <Row>
-
-        {userArray.map((user, i) => {
-          const profileUrl = user.profileUrl
-          const nickname = user.nickname
-          return (
-            <Col key={i}>
-              <UserCard faceImg={profileUrl} nickname={nickname} uuid={uuid} index={i}></UserCard>
-            </Col>
-          )
-        })}
-        {/* {(userArray.length < 4) &&
-          <Col>
-            <Button onClick={() => setShowNewUserModal(true)} variant='secondary' style={{ fontSize: '25px', height: '80px', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              새 프로필 추가하기
-            </Button>
-          </Col>} */}
-        {[...Array(4 - userArray.length)].map((e, i) => {
-          return (
-            <Col key={i} style={{ minHeight: '55vh' }}>
-              <Button onClick={() => setShowNewUserModal(true)} variant='secondary' style={{ fontSize: '30vh', height: '80px', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', opacity: '50%' }}>
-                +
-              </Button>
-            </Col>
-          )
-        })}
-      </Row>
-      <Row style={{ justifyContent: 'center', alignContent: 'center', textAlign: 'center' }}>
-        {userArray.length > 0 ? <Button size='lg' onClick={() => setShowFaceRecogModal(true)} style={{ margin: '5px', width: '80%' }}>얼굴 인식하기</Button> : null}
+        <Col style={{ minHeight: '55vh', alignContent: 'center', justifyContent: 'center', textAlign: 'center' }}>
+          <Spinner animation='border' style={{ width: '100px', height: '100px', margin: 'auto' }} />
+        </Col>
       </Row>
     </Container>
   )
+  else {
+    return (
+      <Container>
+        <NewUserModal show={showNewUserModal} handleClose={() => setShowNewUserModal(false)} />
+        <FaceRecogModal show={showFaceRecogModal} handleClose={() => setShowFaceRecogModal(false)} userArray={userArray} />
+        <Row>
+
+          {userArray.map((user, i) => {
+            const profileUrl = user.profileUrl
+            const nickname = user.nickname
+            return (
+              <Col key={i}>
+                <UserCard faceImg={profileUrl} nickname={nickname} uuid={uuid} index={i}></UserCard>
+              </Col>
+            )
+          })}
+
+          {[...Array(4 - userArray.length)].map((e, i) => {
+            return (
+              <Col key={i} style={{ minHeight: '55vh' }}>
+                <Button onClick={() => setShowNewUserModal(true)} variant='secondary' style={{ fontSize: '30vh', height: '80px', width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', opacity: '50%' }}>
+                  +
+                </Button>
+              </Col>
+            )
+          })}
+        </Row>
+        <Row style={{ justifyContent: 'center', alignContent: 'center', textAlign: 'center' }}>
+          {userArray.length > 0 ? <Button size='lg' onClick={() => setShowFaceRecogModal(true)} style={{ margin: '5px', width: '80%' }}>얼굴 인식하기</Button> : null}
+        </Row>
+      </Container>
+    )
+  }
 }
